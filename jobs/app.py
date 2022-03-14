bd44b4fccbf7e6c8d871fff94f143ef81e63c28e
William Cuthbertson <whc1315@gmail.com>
2022-03-14 09:28:25 -0700

    dynamic1.2

--- jobs/app.py
+++ jobs/app.py
@@ -1,9 +1,15 @@
+from multiprocessing import connection
 import sqlite3
 from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
+
+
+def open_connection():
+    getattr(g, '_connection')
+    return getattr(connection)
 
 
 @app.route('/')
