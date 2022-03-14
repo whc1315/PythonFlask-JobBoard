8a873a0aef3efd998e75330dfb2f39059231c87a
William Cuthbertson <whc1315@gmail.com>
2022-03-14 10:03:17 -0700

    dynamic2.2

--- jobs/app.py
+++ jobs/app.py
@@ -19,6 +19,11 @@ def open_connection():
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    return results
 
 
 @app.route('/')
