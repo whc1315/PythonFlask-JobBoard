4203ff4381cb7692661e10c7fd1ac1e6f798ef2b
William Cuthbertson <whc1315@gmail.com>
2022-03-14 15:01:23 -0700

    review1.0

--- jobs/app.py
+++ jobs/app.py
@@ -58,3 +58,8 @@ def employer(employer_id):
     reviews = execute_sql(
         'SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
     return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
+
+
+@app.route('/employer/<employer_id>/review', methods=('GET', 'POST'))
+def review(employer_id):
+    return render_template('review.html', employer_id=employer_id)
