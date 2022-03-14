f833d1a6d8bac6ac7a94770456e9c889e2a2860a
William Cuthbertson <whc1315@gmail.com>
2022-03-14 14:52:00 -0700

    employer1.9

--- jobs/app.py
+++ jobs/app.py
@@ -53,4 +53,6 @@ def job(job_id):
 def employer(employer_id):
     employer = execute_sql(
         'SELECT * FROM employer WHERE id=?', [employer_id], single=True)
+    jobs = execute_sql(
+        'SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
     return render_template('employer.html', employer=employer)
