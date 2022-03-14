1ea2f359533c039c2bc061e0b04acf6f2624bf2d
William Cuthbertson <whc1315@gmail.com>
2022-03-14 13:16:06 -0700

    indJobs1.3

--- jobs/app.py
+++ jobs/app.py
@@ -44,5 +44,6 @@ def jobs():
 
 @app.route('/job/<job_id>')
 def job(job_id):
-    execute_sql()
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [
+                      job_id], single=True)
     return render_template('job.html')
