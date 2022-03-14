feaa5c8925031ab4e0ca38e58e67ff3ff7343c77
William Cuthbertson <whc1315@gmail.com>
2022-03-14 09:46:11 -0700

    dynamic1.6

--- jobs/app.py
+++ jobs/app.py
@@ -12,6 +12,7 @@ def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
 
 
 @app.route('/')
