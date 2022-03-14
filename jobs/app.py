fc8e33fce3499b4500432e9347625eefeadf8dac
William Cuthbertson <whc1315@gmail.com>
2022-03-14 14:52:46 -0700

    employer2.0

--- jobs/app.py
+++ jobs/app.py
@@ -55,4 +55,4 @@ def employer(employer_id):
         'SELECT * FROM employer WHERE id=?', [employer_id], single=True)
     jobs = execute_sql(
         'SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
-    return render_template('employer.html', employer=employer)
+    return render_template('employer.html', employer=employer, jobs=jobs)
